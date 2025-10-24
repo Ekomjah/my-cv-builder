05ca64b1825079d0ed6cbe7c6e99535e7f0f4461
Ekomjah <ekomjahedet@gmail.com>
2025-10-25 00:20:37 +0100

    refactor: configure tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
   return (
     <>
       <div>
@@ -25,11 +24,12 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs text-lg">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="text-blue-500">Hello, World!</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
